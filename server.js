fe8e014724f371f90e0c8aa8bb196d8c02fb775d
ArunKumarS2504 <149867644+ArunKumarS2504@users.noreply.github.com>
2024-07-15 19:41:04 +0530

    flight

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const nodemailer = require('nodemailer')
+const bodyparser = require('body-parser')
+const pdfDocument = require('pdfkit')
+
+const app = express();
+app.use(bodyparser.json());
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost:27017/flightbooking",{useNewurlParser:true, useUnifiedTopology:true})
+
+app.listen(PORT,()=>{
+    console.log(`server connected to the port ${PORT}`)
+})
+
+app.get("/",(req,res)=>{
+    res.send('Flight-Booking System');
+});
+
